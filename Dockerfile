3bfe10a1615df2c48f065ae5f6a1101f9fb36fb1
zjhans <zacharyjhanson@gmail.com>
2019-12-02 19:59:58 +0000

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+#Start the build phase of our Docker deployment
+FROM node:alpine as builder
+
+WORKDIR '/app'
+
+COPY package.json .
+
+RUN npm install
+
+COPY . .
+
+RUN npm run build
+
+#Start the run phase of our Docker deplyment
+FROM nginx
+
+COPY --from=builder /app/build /usr/share/nginx/html
---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          I edited <code>src/App.js</code> and saved it but it did not show up when I reloaded. I bet it will now, and so will this. And just to prove           a point, I will add some more text.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
